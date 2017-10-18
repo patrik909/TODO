43c68d34fb3d604483349afc000c035198da1e95
Patrik Eriksson <Patrik@Patriks-MacBook-Pro-2.local>
2017-10-18 10:29:08 +0200

    added check file

---
+++ check.php
@@ -0,0 +1,17 @@
+<?php
+
+//header('Location: http://localhost:8888/uppgift2/') ;
+
+//require 'fetchall.php'
+    
+    //$statement = $todo->prepare(
+      //  ""
+    //);
+
+    //$statement->execute(array(
+     //   "1" => $_POST["your_check"]
+    //));
+
+//UPDATE `TODO` SET `completed` = '1' WHERE `TODO`.`id` = $_POST["your_check"]
+
+//UPDATE `TODO` SET `completed` = '1' WHERE `TODO`.`id` = 13;
