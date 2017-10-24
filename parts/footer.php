43ff644b173a65ddaeab0f050732094dc820046c
Patrik Eriksson <Patrik@Patriks-MacBook-Pro-2.local>
2017-10-24 11:31:24 +0200

    Final commit

--- parts/footer.php
+++ parts/footer.php
@@ -1,7 +1,7 @@
-    <footer>
-    
-    </footer>
-    </div>
-    
-</body>
+            <footer>
+                <a href="https://github.com/patrik909/TODO"><i class="fa fa-github" aria-hidden="true"></i> patrik909</a>
+            </footer>
+        </div>
+
+    </body>
 </html>
